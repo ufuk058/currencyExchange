3706b7919a783d6fdd60ae47f1850d3fe46816be
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-12-05 11:29:32 +0000

    initial commit

---
+++ src/main/java/com/currency/RestCurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCurrencyExchangeApplication.class, args);
+	}
+
+}
